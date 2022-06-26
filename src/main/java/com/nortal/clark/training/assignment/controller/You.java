74149845e1a89dd477176432a152b251bd80d3ea
Andre Luige <andreluige@hot.ee>
2022-06-26 12:10:56 +0300

    code cleanup and comments

--- src/main/java/com/nortal/clark/training/assignment/controller/You.java
+++ src/main/java/com/nortal/clark/training/assignment/controller/You.java
@@ -12,12 +12,15 @@ import java.util.List;
 public class You {
 
     private List<Position> targetsToCapture;
+    public static final int MAGIC_RANGE = 2;
+    // public static final double WATER_DRAG = 1.6;
 
     public VoiceCommand getNextStep(Clark clark, CityMap cityMap) {
         VoiceCommand voiceCommand = new VoiceCommand(Direction.SOUTH, SpeedLevel.L0_RUNNING_HUMAN);
 
         if (targetsToCapture == null) {
             targetsToCapture = cityMap.getTargets();
+            // sortPositions();
         }
 
         Position targetToCapture = targetsToCapture.get(0);
@@ -36,24 +39,19 @@ public class You {
         int diffX = Math.abs(targetToCapture.x - clark.getPosition().x);
         int diffY = Math.abs(targetToCapture.y - clark.getPosition().y);
 
-        SpeedLevel horizontalSpeedLevel = thinkOfSpeedLevel(diffX, clark.getHorizontal());
-        SpeedLevel verticalSpeedLevel = thinkOfSpeedLevel(diffY, clark.getVertical());
+        SpeedLevel horizontalSpeedLevel = thinkOfSpeedLevel(diffX);
+        SpeedLevel verticalSpeedLevel = thinkOfSpeedLevel(diffY);
 
-        if (diffX < 2 && diffY < 2) {
+        if (diffX < MAGIC_RANGE && diffY < MAGIC_RANGE) {
             System.out.println("Removing target");
             targetsToCapture.remove(index); //Consider it captured
-
-        } else if (targetToCapture.x > clark.getPosition().x && diffX > 2) {
-            if (diffX > 15) {
-                voiceCommand = new VoiceCommand(Direction.EAST, horizontalSpeedLevel);
-            } else {
-                voiceCommand = new VoiceCommand(Direction.EAST, SpeedLevel.L3_SUPER_SONIC);
-            }
-        } else if (targetToCapture.x < clark.getPosition().x && diffX > 2) {
+        } else if (targetToCapture.x > clark.getPosition().x && diffX > MAGIC_RANGE) {
+            voiceCommand = (diffX > 15) ? new VoiceCommand(Direction.EAST, horizontalSpeedLevel) : new VoiceCommand(Direction.EAST, SpeedLevel.L3_SUPER_SONIC);
+        } else if (targetToCapture.x < clark.getPosition().x && diffX > MAGIC_RANGE) {
             voiceCommand = new VoiceCommand(Direction.WEST, horizontalSpeedLevel);
-        } else if (targetToCapture.y > clark.getPosition().y && diffY > 2) {
+        } else if (targetToCapture.y > clark.getPosition().y && diffY > MAGIC_RANGE) {
             voiceCommand = new VoiceCommand(Direction.NORTH, verticalSpeedLevel);
-        } else if (targetToCapture.y < clark.getPosition().y && diffY > 2) {
+        } else if (targetToCapture.y < clark.getPosition().y && diffY > MAGIC_RANGE) {
             voiceCommand = new VoiceCommand(Direction.SOUTH, verticalSpeedLevel);
         }
 
@@ -61,7 +59,7 @@ public class You {
         return voiceCommand;
     }
 
-    private SpeedLevel thinkOfSpeedLevel(int distanceDiff, double speed) {
+    private SpeedLevel thinkOfSpeedLevel(int distanceDiff) {
         if (distanceDiff > 45)
             return SpeedLevel.L4_MACH_9350;
         if (distanceDiff > 10)
@@ -69,4 +67,24 @@ public class You {
 
         return SpeedLevel.L3_SUPER_SONIC;
     }
+
+    // Tried Dijkstra shortest path algorithm, but did not give faster execution in test.
+    /*
+    private void sortPositions() {
+        targetsToCapture.sort((Position lhs, Position rhs) -> {
+            double d1 = Math.hypot(lhs.x, lhs.y);
+            double d2 = Math.hypot(rhs.x, rhs.y);
+            return Double.compare(d1, d2);
+        });
+    }
+    */
+
+    // Tried to use water drag factor to make smoothen out turns, but execution time suffered.
+    /*
+    private double getDragAcceleration(double currentSpeed) {
+        double dragDirectionalModifier = -Math.signum(currentSpeed);
+        double waterDrag = WATER_DRAG + (Math.pow(currentSpeed, 2) / 200);
+        return dragDirectionalModifier * waterDrag;
+    }
+    */
 }
